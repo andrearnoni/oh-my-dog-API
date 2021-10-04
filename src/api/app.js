e8a906ff701e3ac89e32bc32c7dccf0b06947f35
André Arnoni <andre_arnoni@yahoo.com.br>
2021-10-04 17:34:34 -0300

    adicionado cors e texto teste do readme

--- src/api/app.js
+++ src/api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
@@ -6,5 +7,9 @@ const { upload } = require('../../middlewares/multer');
 
 app.use(express.json());
 app.use('/images', express.static(path.resolve('src/uploads')));
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "Olá mundo" })
+})
 
 module.exports = app;
